526f6389f11e2a6bc83137750b19ac1682a77b2c
Khasugha <swaanslouis@qq.com>
2025-01-22 15:29:23 +0700

    beautify

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    # print(f"{num_words} words found in the document")
-    get_num_characters(text)
+    chars_dict = get_num_characters(text)
+    print(chars_dict)
 
 def get_num_characters(text):
     lowered_text = text.lower()
